fbeaf3815b9b37c111770477fcf0c62b5dcb46a2
deeksha-1720 <deekshanagarajan55@gmail.com>
2026-02-24 03:30:35 +0530

    Added UC6

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,24 +1,54 @@
 public class OOPSBannerApp {
-     public static void main(String[] args){
-        String[] lines = {
 
-            String.join(" ",
-                    " ***  ", " ***  ", " ***** ", " ***** "),
-            String.join(" ",
-                    "**  **", "**  **", "**  **", "**  "),
-            String.join(" ",
-                    "**  **", "**  **", "**  **", "**  "),
-            String.join(" ",
-                    "**  **", "**  **", "******", "  **** "),
-            String.join(" ",
-                    "**  **", "**  **", "**     ", "    ** "),
-            String.join(" ",
-                    "**  **", "**  **", "**     ", "    **  "),
-            String.join(" ",
-                    " ***  ", " ***  ", "**     ", " ***** ")
+    public static String[] getOPattern() {
+        return new String[]{
+                "   ***   ",
+                " **   ** ",
+                "**     **",
+                "**     **",
+                "**     **",
+                "**     **",
+                "**     **",
+                " **   ** ",
+                "   ***   "
         };
-        for (String line : lines) {
-            System.out.println(line);
+    }
+
+    public static String[] getPPattern() {
+        return new String[]{
+                "******   ",
+                "**    ** ",
+                "**     **",
+                "**    ** ",
+                "******   ",
+                "**       ",
+                "**       ",
+                "**       ",
+                "**       "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+                "   *****",
+                " **",
+                "**",
+                " **",
+                "  ***",
+                "     **",
+                "      **",
+                "     **",
+                "*****"
+        };
+    }
+    public static void main(String[] args) {
+
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + " " + o[i] + " " + p[i] + " " + s[i]);
         }
     }
 }
